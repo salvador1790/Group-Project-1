73966fa9eb9ef3d2813000f1565d532503536ee3
Michael Rossi <michaelrossi@Michaels-MacBook-Pro.local>
2019-07-06 13:30:15 -0400

    Index and basic add book pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,49 @@
 //Initialize Firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyA4RlrYXgKvMYzZQj8p9Ls9s6YZB9mAfXA",
-    authDomain: "bookshlf-61afa.firebaseapp.com",
-    databaseURL: "https://bookshlf-61afa.firebaseio.com",
-    projectId: "bookshlf-61afa",
-    storageBucket: "",
-    messagingSenderId: "427376747607",
-    appId: "1:427376747607:web:3cd338bd21f19ee8"
-  };
- 
-  firebase.initializeApp(firebaseConfig);
-  
-  var database = firebase.database();
+  apiKey: "AIzaSyA4RlrYXgKvMYzZQj8p9Ls9s6YZB9mAfXA",
+  authDomain: "bookshlf-61afa.firebaseapp.com",
+  databaseURL: "https://bookshlf-61afa.firebaseio.com",
+  projectId: "bookshlf-61afa",
+  storageBucket: "",
+  messagingSenderId: "427376747607",
+  appId: "1:427376747607:web:3cd338bd21f19ee8"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
 
 //Create Profile Button
-$("#add-member-btn").on("click", function(event) {
-    event.preventDefault();
-  
-    // Get data from UI
-    var name = $("#member-name-input").val().trim();
-    var email = $("#member-email-input").val().trim();
-    var phone = $("#member-phone-input").val().trim();
-    var street = $("#member-street-input").val().trim();
-    var city = $("#member-city-input").val().trim();
-    var state = $("#member-state-input").val().trim();
-    var zip = $("#member-zip-input").val().trim();
-  
-    // Creates local object
-    var newMember = {
-      name,
-      email,
-      phone,
-      street,
-      city,
-      state,
-      zip
-    };
-  
-    // Uploads member data to Firebase
-    database.ref().push(newMember);
-  
-    alert("Profile successfully added");
+$("#add-member-btn").on("click", function (event) {
+  event.preventDefault();
 
-  });
+  // Get data from UI
+  var name = $("#member-name-input").val().trim();
+  var email = $("#member-email-input").val().trim();
+  var phone = $("#member-phone-input").val().trim();
+  var street = $("#member-street-input").val().trim();
+  var city = $("#member-city-input").val().trim();
+  var state = $("#member-state-input").val().trim();
+  var zip = $("#member-zip-input").val().trim();
+
+  // Creates local object
+  var newMember = {
+    name,
+    email,
+    phone,
+    street,
+    city,
+    state,
+    zip
+  };
+
+  // Uploads member data to Firebase
+  database.ref("users").push(newMember);
+
+  alert("Profile successfully added");
+
+});
+
+
+
